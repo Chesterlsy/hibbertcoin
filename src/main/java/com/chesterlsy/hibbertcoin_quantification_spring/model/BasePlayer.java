57831d22981048ea52d188e06d67119c47ac10a0
Chesterlsy <siyiliu641@hotmail.com>
2019-02-19 11:21:14 +0800

    Basic structure.

---
+++ src/main/java/com/chesterlsy/hibbertcoin_quantification_spring/model/BasePlayer.java
@@ -0,0 +1,38 @@
+package com.chesterlsy.hibbertcoin_quantification_spring.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@JsonIgnoreProperties(value = {"createdAt", "updatedAt"}, allowGetters = true)
+@Getter @Setter @NoArgsConstructor
+public abstract class BasePlayer implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter(AccessLevel.PROTECTED)
+    private Long id;
+
+    @NotBlank
+    private String name;
+
+    @Column(nullable = false, updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @CreatedDate
+    private Date createdAt;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @LastModifiedDate
+    private Date updatedAt;
+
+}

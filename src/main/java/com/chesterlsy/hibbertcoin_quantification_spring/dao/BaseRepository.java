1c1f0743dd5a2ee0160ccf875814157782c09fc5
Chesterlsy <siyiliu641@hotmail.com>
2019-02-25 10:48:06 +0800

    repo

--- src/main/java/com/chesterlsy/hibbertcoin_quantification_spring/dao/BaseRepository.java
+++ src/main/java/com/chesterlsy/hibbertcoin_quantification_spring/dao/BaseRepository.java
@@ -1,10 +1,8 @@
-package com.chesterlsy.hibbertcoin_quantification_spring.repository;
+package com.chesterlsy.hibbertcoin_quantification_spring.dao;
 
 import com.chesterlsy.hibbertcoin_quantification_spring.model.BaseModel;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.NoRepositoryBean;
-import org.springframework.stereotype.Repository;
 
 @NoRepositoryBean
 public interface BaseRepository<T extends BaseModel, ID extends Long> extends JpaRepository<T, ID> {
--- src/main/java/com/chesterlsy/hibbertcoin_quantification_spring/dao/PlayerRepository.java
+++ src/main/java/com/chesterlsy/hibbertcoin_quantification_spring/dao/PlayerRepository.java
@@ -1,9 +1,9 @@
-package com.chesterlsy.hibbertcoin_quantification_spring.repository;
+package com.chesterlsy.hibbertcoin_quantification_spring.dao;
 
 import com.chesterlsy.hibbertcoin_quantification_spring.model.Player;
+import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
+@Repository
 public interface PlayerRepository extends BaseRepository<Player, Long> {
 
     Player findByName(String name);
